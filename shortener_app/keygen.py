cd5b83a49f0594e2fe18572e45030677ea55f60f
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-25 19:32:45 -0400

    uploaded two more files to reduce and make it efficent, bugs in main

---
+++ shortener_app/keygen.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+from . import crud
+import secrets
+import string
+
+def create_random_key(length: int = 5) -> str:
+    chars = string.ascii_uppercase + string.digits
+    return "".join(secrets.choice(chars) for _ in range(length))
