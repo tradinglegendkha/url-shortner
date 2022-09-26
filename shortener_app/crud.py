91aff626857766fba13198cb5a3d48778d64ead9
Buppi Khan <buppikhan@Buppis-MacBook-Air.local>
2022-09-26 01:06:16 -0400

    added click or viewer counter

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from . import keygen, models, schemas
 
 def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
-    key = keygen.create_unique_random_key(db)
+    key = keygen.create_unique_random_key(db) #make sure that they are no two dubplicate keys
     secret_key = f"{key}_{keygen.create_random_key(length=8)}"
     db_url = models.URL (
         target_url = url.target_url, key=key, secret_key=secret_key
@@ -12,10 +12,16 @@ def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
     db.commit()
     db.refresh(db_url)
     return db_url
-
+#admin key for the url information 
 def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
     return (
         db.query(models.URL)
         .filter(models.URL.secret_key == secret_key, models.URL.is_active)
         .first()
     )
+
+def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+    db_url.clicks += 1 
+    db.commit()
+    db.refresh(db_url)
+    return db_url
